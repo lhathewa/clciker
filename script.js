32b0660b35c63f17953aa3a3b7365dda11111318
lhathewa <60928829+lhathewa@users.noreply.github.com>
2021-03-21 22:40:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var fire=3;
+var ice=1;
+var posion=4;
+var dragon=0;
+
+var wrap = document.body.querySelector(".wrapper");
+
+function boxMaker(nme){
+var box = document.createElement("div");
+box.innerHTML=nme;
+wrap.appendChild(box);
+
+}
+document.body.querySelector("butts").addEventListener("click",funcion(){})
+
+for(var i=0; i<data.length; i++){
+  boxMaker(data[i]);
+ 
+}
+
+ document.body.querySelector="Dragon's Damage "+dragon;
